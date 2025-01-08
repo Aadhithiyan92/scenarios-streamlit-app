f4a32e1cfcb69f4545f666c2bf394d82d362158c
AADHITHIYAN SUBRAMANIYAN <146747894+Aadhithiyan92@users.noreply.github.com>
2025-01-08 14:34:33 -0500

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -198,8 +198,8 @@ def run_scenario(base_params, incentive_ratio, growth_ratio, total_budget=5e9, y
 scenarios = [
     ("70-30 Growth Focus", 0.3, 0.7),
     ("70-30 Incentive Focus", 0.7, 0.3),
-    ("70-30 Growth Focus", 0.4, 0.6),
-    ("70-30 Incentive Focus", 0.6, 0.4),
+    ("60-40 Growth Focus", 0.4, 0.6),
+    ("60-40 Incentive Focus", 0.6, 0.4),
     ("50-50 Growth Focus", 0.5, 0.5),
     
 ]
